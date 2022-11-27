f0139d13f9274b64d3d89d4b5cc5ff52a5eb860b
selimalahi <alahi.selim@gmail.com>
2022-11-27 10:34:05 +0600

    basic setup mongodb

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tlsdvvb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+app.get('/', async(req, res) =>{
+    res.send('resale server is running');
+})
+
+app.listen(port, () => console.log(`Resale portal running on ${port}`))
