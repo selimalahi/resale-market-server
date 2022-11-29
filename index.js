5c05b2e3972f6f34d43feb1e703a76a63897fcfe
selimalahi <alahi.selim@gmail.com>
2022-11-29 11:20:05 +0600

    get the all user

--- index.js
+++ index.js
@@ -10,13 +10,36 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
-
+ 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tlsdvvb.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+
+
+
+function verifyJWT(req, res, next) {
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access');
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
+
+
 
 async function run() {
   try {
@@ -57,13 +80,39 @@ async function run() {
     })
 
     // my booking 
-    app.get('/bookings', async (req, res) =>{
+    app.get('/bookings', verifyJWT, async (req, res) =>{
         const email = req.query.email;
+        const decodedEmail = req.decoded.email;
+
+        if (email !== decodedEmail) {
+            return res.status(403).send({ message: 'forbidden access' });
+        }
+    
         const query ={ email: email };
         const booking = await bookingsCollection.find(query).toArray();
         res.send(booking);
     })
 
+    // get all users    
+    app.get('/users', async (req, res) => {
+        const query = {};
+        const users = await usersCollection.find(query).toArray();
+        res.send(users);
+    });
+     
+
+    // create token
+
+    app.get('/jwt', async (req, res) => {
+        const email = req.query.email;
+        const query = { email: email };
+        const user = await usersCollection.findOne(query);
+        if (user) {
+            const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+            return res.send({ accessToken: token });
+        }
+        res.status(403).send({ accessToken: '' })
+    });
 
     app.post('/users', async (req, res) => {
         const user = req.body;
